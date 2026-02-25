8326d7cfb7cf87d35608fbe47bc1157993f2d7c7
Rishabh Raj <rishabh.raj1209@gmail.com>
2026-02-25 15:13:54 +0530

    Fix ambiguous RequestBody import in AuthController

--- backend/src/main/java/com/carebridge/controller/AuthController.java
+++ backend/src/main/java/com/carebridge/controller/AuthController.java
@@ -2,10 +2,17 @@ package com.carebridge.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import okhttp3.*;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Map;
 
